c46f29cce195d1355971ad8de35cc5d128f43625
Iyasu2 <iyasuforreal@gmail.com>
2023-07-21 09:41:47 +0300

    bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+#include <stdio.h>
+/**
+ * bubble_sort - Entry point
+ * Description: 'sorting using bubble sort algorithm'
+ *
+ * @size: size_t input
+ * @array: int input
+ * Return: none
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i;
+	size_t j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
