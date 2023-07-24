2b78e171936d89b1c6be0c803bde89d0c616d7dd
iyasu2 <iyasuforreal@gmail.com>
2023-07-24 16:06:11 +0300

    betty

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,26 +8,25 @@
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i, j;
-    int temp;
+	size_t gap = 1, i, j;
+	int temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    while (gap < size / 3)
-        gap = gap * 3 + 1;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
 
-    while (gap > 0)
-    {
-        for (i = gap; i < size; i++)
-        {
-            temp = array[i];
-            for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-                array[j] = array[j - gap];
-            array[j] = temp;
-        }
-        print_array(array, size);
-        gap /= 3;
-    }
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+				array[j] = array[j - gap];
+			array[j] = temp;
+		}
+		print_array(array, size);
+		gap /= 3;
+	}
 }
-
